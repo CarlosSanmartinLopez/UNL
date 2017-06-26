cbad8fe0229dd24a2a61a1748443ea60c988d710
Andres <andres.stelliox@gmail.com>
2017-06-26 13:19:42 -0500

    .

---
+++ UNL4/PHP/login/images.php
@@ -0,0 +1,22 @@
+<?php if(!defined('ACCESS')) header('Location: .'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Imagenes</title>
+</head>
+<body>
+<div>
+	<div>
+		<img src="http://4.bp.blogspot.com/-o25O-yxbklU/UQl7cBx0jzI/AAAAAAAAAWM/2j_MXsa7ucs/s1600/picto-personas.jpg">
+	</div>
+	<div>
+		<p>Titulo: Personas</p>
+		<p>Ubicacion: Desconosida</p>
+		<p>Comentario: Grupo de personas</p>
+	</div>
+
+</div>
+
+</body>
+</html>
